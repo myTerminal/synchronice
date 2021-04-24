bf1f6706e4422be4f58635b9546e7fc99c881feb
myTerminal <team.terminal@gmail.com>
2021-04-24 12:30:49 -0500

    designate a separate cache variable for Events

--- src/cache.rs
+++ src/cache.rs
@@ -1,6 +1,9 @@
 //! Holds static data for cache.
 
-use crate::service::Config;
+use crate::service::{Config, Events};
 
 // Holds config fetched from service
 pub static mut CONFIG: Vec<Config> = vec![];
+
+// Holds events fetched from service
+pub static mut EVENTS: Vec<Events> = vec![];
